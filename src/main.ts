f8b750e55f700af61d96160c5e03721a801d796c
LeVanTai19 <levantai2106tv@gmail.com>
2026-06-11 02:59:33 +0700

    fix: port for deployment

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,6 @@ async function bootstrap() {
   // teamplate engine dùng là handlebars (hbs)
   app.setViewEngine('hbs');
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
